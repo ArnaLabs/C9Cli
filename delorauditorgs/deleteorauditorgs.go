66c27ae5b392849fc79bd59a849cc6ecefd462f1
kartikeyavadlapatla <vkartheek.ky1311@gmail.com>
2022-05-16 11:40:41 -0400

    Create deleteorauditorgs.go

---
+++ delorauditorgs/deleteorauditorgs.go
@@ -0,0 +1,635 @@
+package delorauditorgs
+
+import (
+	"encoding/json"
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os/exec"
+	"regexp"
+	"strings"
+	"time"
+)
+
+type SpaceListJson struct {
+	Pagination struct {
+		TotalResults int `json:"total_results"`
+		TotalPages   int `json:"total_pages"`
+		First        struct {
+			Href string `json:"href"`
+		} `json:"first"`
+		Last struct {
+			Href string `json:"href"`
+		} `json:"last"`
+		Next     interface{} `json:"next"`
+		Previous interface{} `json:"previous"`
+	} `json:"pagination"`
+	Resources []struct {
+		GUID          string    `json:"guid"`
+		CreatedAt     time.Time `json:"created_at"`
+		UpdatedAt     time.Time `json:"updated_at"`
+		Name          string    `json:"name"`
+		Relationships struct {
+			Organization struct {
+				Data struct {
+					GUID string `json:"guid"`
+				} `json:"data"`
+			} `json:"organization"`
+			Quota struct {
+				Data interface{} `json:"data"`
+			} `json:"quota"`
+		} `json:"relationships"`
+		Links struct {
+			Self struct {
+				Href string `json:"href"`
+			} `json:"self"`
+			Organization struct {
+				Href string `json:"href"`
+			} `json:"organization"`
+			Features struct {
+				Href string `json:"href"`
+			} `json:"features"`
+			ApplyManifest struct {
+				Href   string `json:"href"`
+				Method string `json:"method"`
+			} `json:"apply_manifest"`
+		} `json:"links"`
+		Metadata struct {
+			Labels struct {
+			} `json:"labels"`
+			Annotations struct {
+			} `json:"annotations"`
+		} `json:"metadata"`
+	} `json:"resources"`
+}
+type OrgListJson struct {
+	Pagination struct {
+		TotalResults int `json:"total_results"`
+		TotalPages   int `json:"total_pages"`
+		First        struct {
+			Href string `json:"href"`
+		} `json:"first"`
+		Last struct {
+			Href string `json:"href"`
+		} `json:"last"`
+		Next     interface{} `json:"next"`
+		Previous interface{} `json:"previous"`
+	} `json:"pagination"`
+	Resources []struct {
+		GUID          string    `json:"guid"`
+		CreatedAt     time.Time `json:"created_at"`
+		UpdatedAt     time.Time `json:"updated_at"`
+		Name          string    `json:"name"`
+		Suspended     bool      `json:"suspended"`
+		Relationships struct {
+			Quota struct {
+				Data struct {
+					GUID string `json:"guid"`
+				} `json:"data"`
+			} `json:"quota"`
+		} `json:"relationships"`
+		Links struct {
+			Self struct {
+				Href string `json:"href"`
+			} `json:"self"`
+			Domains struct {
+				Href string `json:"href"`
+			} `json:"domains"`
+			DefaultDomain struct {
+				Href string `json:"href"`
+			} `json:"default_domain"`
+			Quota struct {
+				Href string `json:"href"`
+			} `json:"quota"`
+		} `json:"links"`
+		Metadata struct {
+			Labels struct {
+			} `json:"labels"`
+			Annotations struct {
+			} `json:"annotations"`
+		} `json:"metadata"`
+	} `json:"resources"`
+}
+type QuotaListJson struct {
+	Pagination struct {
+		TotalResults int `json:"total_results"`
+		TotalPages   int `json:"total_pages"`
+		First        struct {
+			Href string `json:"href"`
+		} `json:"first"`
+		Last struct {
+			Href string `json:"href"`
+		} `json:"last"`
+		Next     interface{} `json:"next"`
+		Previous interface{} `json:"previous"`
+	} `json:"pagination"`
+	Resources []struct {
+		GUID      string    `json:"guid"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Name      string    `json:"name"`
+		Apps      struct {
+			TotalMemoryInMb      int         `json:"total_memory_in_mb"`
+			PerProcessMemoryInMb interface{} `json:"per_process_memory_in_mb"`
+			TotalInstances       int         `json:"total_instances"`
+			PerAppTasks          interface{} `json:"per_app_tasks"`
+		} `json:"apps"`
+		Services struct {
+			PaidServicesAllowed   bool        `json:"paid_services_allowed"`
+			TotalServiceInstances int         `json:"total_service_instances"`
+			TotalServiceKeys      interface{} `json:"total_service_keys"`
+		} `json:"services"`
+		Routes struct {
+			TotalRoutes        interface{} `json:"total_routes"`
+			TotalReservedPorts int         `json:"total_reserved_ports"`
+		} `json:"routes"`
+		Domains struct {
+			TotalDomains interface{} `json:"total_domains"`
+		} `json:"domains"`
+		Relationships struct {
+			Organizations struct {
+				Data []struct {
+					GUID string `json:"guid"`
+				} `json:"data"`
+			} `json:"organizations"`
+		} `json:"relationships"`
+		Links struct {
+			Self struct {
+				Href string `json:"href"`
+			} `json:"self"`
+		} `json:"links"`
+	} `json:"resources"`
+}
+type OrgUsersListJson struct {
+	Pagination struct {
+		TotalResults int `json:"total_results"`
+		TotalPages   int `json:"total_pages"`
+		First        struct {
+			Href string `json:"href"`
+		} `json:"first"`
+		Last struct {
+			Href string `json:"href"`
+		} `json:"last"`
+		Next struct {
+			Href string `json:"href"`
+		} `json:"next"`
+		Previous interface{} `json:"previous"`
+	} `json:"pagination"`
+	Resources []struct {
+		GUID          string    `json:"guid"`
+		CreatedAt     time.Time `json:"created_at"`
+		UpdatedAt     time.Time `json:"updated_at"`
+		Type          string    `json:"type"`
+		Relationships struct {
+			User struct {
+				Data struct {
+					GUID string `json:"guid"`
+				} `json:"data"`
+			} `json:"user"`
+			Organization struct {
+				Data struct {
+					GUID string `json:"guid"`
+				} `json:"data"`
+			} `json:"organization"`
+			Space struct {
+				Data interface{} `json:"data"`
+			} `json:"space"`
+		} `json:"relationships"`
+	} `json:"resources"`
+}
+type UserDetailsJson struct {
+	Pagination struct {
+		TotalResults int `json:"total_results"`
+		TotalPages   int `json:"total_pages"`
+		First        struct {
+			Href string `json:"href"`
+		} `json:"first"`
+		Last struct {
+			Href string `json:"href"`
+		} `json:"last"`
+		Next     interface{} `json:"next"`
+		Previous interface{} `json:"previous"`
+	} `json:"pagination"`
+	Resources []struct {
+		GUID             string    `json:"guid"`
+		CreatedAt        time.Time `json:"created_at"`
+		UpdatedAt        time.Time `json:"updated_at"`
+		Username         string    `json:"username"`
+		PresentationName string    `json:"presentation_name"`
+		Origin           string    `json:"origin"`
+		Metadata         struct {
+			Labels struct {
+			} `json:"labels"`
+			Annotations struct {
+			} `json:"annotations"`
+		} `json:"metadata"`
+		Links struct {
+			Self struct {
+				Href string `json:"href"`
+			} `json:"self"`
+		} `json:"links"`
+	} `json:"resources"`
+}
+type SpaceUsersListJson struct {
+	Pagination struct {
+		TotalResults int `json:"total_results"`
+		TotalPages   int `json:"total_pages"`
+		First        struct {
+			Href string `json:"href"`
+		} `json:"first"`
+		Last struct {
+			Href string `json:"href"`
+		} `json:"last"`
+		Next     interface{} `json:"next"`
+		Previous interface{} `json:"previous"`
+	} `json:"pagination"`
+	Resources []struct {
+		GUID          string    `json:"guid"`
+		CreatedAt     time.Time `json:"created_at"`
+		UpdatedAt     time.Time `json:"updated_at"`
+		Type          string    `json:"type"`
+		Relationships struct {
+			User struct {
+				Data struct {
+					GUID string `json:"guid"`
+				} `json:"data"`
+			} `json:"user"`
+			Space struct {
+				Data struct {
+					GUID string `json:"guid"`
+				} `json:"data"`
+			} `json:"space"`
+			Organization struct {
+				Data interface{} `json:"data"`
+			} `json:"organization"`
+		} `json:"relationships"`
+		Links struct {
+			Self struct {
+				Href string `json:"href"`
+			} `json:"self"`
+			User struct {
+				Href string `json:"href"`
+			} `json:"user"`
+			Space struct {
+				Href string `json:"href"`
+			} `json:"space"`
+		} `json:"links"`
+	} `json:"resources"`
+}
+type ASGListJson struct {
+	Pagination struct {
+		TotalResults int `json:"total_results"`
+		TotalPages   int `json:"total_pages"`
+		First        struct {
+			Href string `json:"href"`
+		} `json:"first"`
+		Last struct {
+			Href string `json:"href"`
+		} `json:"last"`
+		Next     interface{} `json:"next"`
+		Previous interface{} `json:"previous"`
+	} `json:"pagination"`
+	Resources []struct {
+		GUID      string    `json:"guid"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Name      string    `json:"name"`
+		Rules     []struct {
+			Destination string `json:"destination"`
+			Protocol    string `json:"protocol"`
+		} `json:"rules"`
+		GloballyEnabled struct {
+			Running bool `json:"running"`
+			Staging bool `json:"staging"`
+		} `json:"globally_enabled"`
+		Relationships struct {
+			RunningSpaces struct {
+				Data []struct {
+					GUID string `json:"guid"`
+				} `json:"data"`
+			} `json:"running_spaces"`
+			StagingSpaces struct {
+				Data []struct {
+					GUID string `json:"guid"`
+				} `json:"data"`
+			} `json:"staging_spaces"`
+		} `json:"relationships"`
+		Links struct {
+			Self struct {
+				Href string `json:"href"`
+			} `json:"self"`
+		} `json:"links"`
+	} `json:"resources"`
+}
+
+type Quotalist struct {
+	Quota []struct {
+		Name        string `yaml:"Name"`
+		MemoryLimit string `yaml:"memory_limit"`
+		AllowPaidPlans       bool `yaml:"allow_paid_plans"`
+		AppInstanceLimit     string `yaml:"app_instance_limit"`
+		ServiceInstanceLimit string `yaml:"service_instance_limit"`
+	} `yaml:"quota"`
+	Audit string `yaml:"Audit"`
+}
+type List struct {
+	OrgList []struct {
+		Name string `yaml:"Name"`
+		Quota string `yaml:"Quota"`
+	} `yaml:"OrgList"`
+	Audit string `yaml:"Audit"`
+}
+type GitList struct {
+	OrgList []struct {
+		Name string `yaml:"Name"`
+		Repo string `yaml:"Repo"`
+		Quota string `yaml:"Quota"`
+		Branch string `yaml:"Branch"`
+	} `yaml:"OrgList"`
+	Audit string `yaml:"Audit"`
+}
+type Orglist struct {
+	Org struct {
+		Name     string `yaml:"Name"`
+		//Quota    string `yaml:"Quota"`
+		OrgUsers struct {
+			LDAP struct {
+				OrgManagers []string `yaml:"OrgManagers"`
+				OrgAuditors []string `yaml:"OrgAuditors"`
+			} `yaml:"LDAP"`
+			SSO struct {
+				OrgManagers []string `yaml:"OrgManagers"`
+				OrgAuditors []string `yaml:"OrgAuditors"`
+			} `yaml:"SSO"`
+			UAA struct {
+				OrgManagers []string `yaml:"OrgManagers"`
+				OrgAuditors []string `yaml:"OrgAuditors"`
+			} `yaml:"UAA"`
+		} `yaml:"OrgUsers"`
+		Spaces []struct {
+			Name         string `yaml:"Name"`
+			IsolationSeg string `yaml:"IsolationSeg"`
+			ASG string `yaml:"ASG"`
+			SpaceUsers   struct {
+				LDAP struct {
+					SpaceManagers   []string `yaml:"SpaceManagers"`
+					SpaceDevelopers []string `yaml:"SpaceDevelopers"`
+					SpaceAuditors   []string `yaml:"SpaceAuditors"`
+				} `yaml:"LDAP"`
+				UAA struct {
+					SpaceManagers   []string `yaml:"SpaceManagers"`
+					SpaceDevelopers []string `yaml:"SpaceDevelopers"`
+					SpaceAuditors   []string `yaml:"SpaceAuditors"`
+				} `yaml:"UAA"`
+				SSO struct {
+					SpaceManagers   []string `yaml:"SpaceManagers"`
+					SpaceDevelopers []string `yaml:"SpaceDevelopers"`
+					SpaceAuditors   []string `yaml:"SpaceAuditors"`
+				} `yaml:"SSO"`
+			} `yaml:"SpaceUsers"`
+		} `yaml:"Spaces"`
+	} `yaml:"Org"`
+	SpaceAudit string `yaml:"SpaceAudit"`
+	UserAudit string `yaml:"UserAudit"`
+	ASGAudit string `yaml:"ASGAudit"`
+	IsolationAudit string `yaml:"IsolationAudit"`
+}
+type ProtectedList struct {
+	Org   []string `yaml:"Org"`
+	Quota []string `yaml:"quota"`
+	DefaultRunningSecurityGroup string   `yaml:"DefaultRunningSecurityGroup"`
+}
+type InitClusterConfigVals struct {
+	ClusterDetails struct {
+		EndPoint  string `yaml:"EndPoint"`
+		User      string `yaml:"User"`
+		Org       string `yaml:"Org"`
+		Space     string `yaml:"Space"`
+		EnableASG bool   `yaml:"EnableASG"`
+		EnableGitSubTree bool `yaml:"EnableGitSubTree"`
+		GitHost string `yaml:"GitHost"`
+		SetOrgAuditor bool	`yaml:"SetOrgAuditor"`
+		SetOrgManager bool	`yaml:"SetOrgManager"`
+		SetSpaceAuditor bool	`yaml:"SetSpaceAuditor"`
+		SetSpaceManager bool	`yaml:"SetSpaceManager"`
+		SetSpaceDeveloper bool	`yaml:"SetSpaceDeveloper"`
+		EnableSpaceAudit bool `yaml:"EnableSpaceAudit"`
+		EnableUserAudit bool `yaml:"EnableUserAudit"`
+		EnableASGAudit bool `yaml:"EnableASGAudit"`
+		EnableIsolationAudit bool `yaml:"EnableIsolationAudit"`
+		SSOProvider string `yaml:"SSOProvider"`
+	} `yaml:"ClusterDetails"`
+}
+type OrgStateYaml struct {
+	OrgState struct {
+		OldName string `yaml:"OldName"`
+		NewName string `yaml:"NewName"`
+		OrgGuid string `yaml:"OrgGuid"`
+	} `yaml:"OrgState"`
+}
+type SpaceStateYaml struct {
+	SpaceState struct {
+		Org     string `yaml:"Org"`
+		OrgGuid string `yaml:"OrgGuid"`
+		OldSpaceName string `yaml:"OldSpaceName"`
+		NewSpaceName string `yaml:"NewSpaceName"`
+		SpaceGuid    string `yaml:"SpaceGuid"`
+	} `yaml:"SpaceState"`
+}
+
+func DeleteorAuditOrgs(clustername string, cpath string) error {
+
+	var list List
+	var ProtectedOrgs ProtectedList
+	var gitlist GitList
+	var InitClusterConfigVals InitClusterConfigVals
+	var ListYml, Audit string
+	var  LenList int
+	ConfigFile := cpath+"/"+clustername+"/config.yml"
+	fileConfigYml, err := ioutil.ReadFile(ConfigFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = yaml.Unmarshal([]byte(fileConfigYml), &InitClusterConfigVals)
+	if err != nil {
+		panic(err)
+	}
+	if InitClusterConfigVals.ClusterDetails.EnableGitSubTree != true {
+		ListYml = cpath+"/"+clustername+"/OrgsList.yml"
+		fileOrgYml, err := ioutil.ReadFile(ListYml)
+		if err != nil {
+			fmt.Println(err)
+		}
+		err = yaml.Unmarshal([]byte(fileOrgYml), &list)
+		if err != nil {
+			panic(err)
+		}
+		LenList = len(list.OrgList)
+		Audit = strings.ToLower(list.Audit)
+		if Audit == "" {
+			Audit = "list"
+		}
+
+
+	} else {
+		ListYml = cpath+"/"+clustername+"/GitOrgsList.yml"
+		fileOrgYml, err := ioutil.ReadFile(ListYml)
+		if err != nil {
+			fmt.Println(err)
+		}
+		err = yaml.Unmarshal([]byte(fileOrgYml), &gitlist)
+		if err != nil {
+			panic(err)
+		}
+		LenList = len(gitlist.OrgList)
+		Audit = strings.ToLower(gitlist.Audit)
+		if Audit == "" {
+			Audit = "list"
+		}
+	}
+
+	ProtectedOrgsYml := cpath+"/"+clustername+"/ProtectedResources.yml"
+	fileProtectedYml, err := ioutil.ReadFile(ProtectedOrgsYml)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = yaml.Unmarshal([]byte(fileProtectedYml), &ProtectedOrgs)
+	if err != nil {
+		panic(err)
+	}
+	LenProtectedOrgs := len(ProtectedOrgs.Org)
+
+	getorgs := exec.Command("cf", "curl", "/v3/organizations", "--output", "DeleteorAuditOrgs_listorgs.json")
+
+	if _, err := getorgs.Output(); err == nil {
+		fmt.Println("command: ", getorgs)
+
+		var body OrgListJson
+
+		fileOrgJson, err := ioutil.ReadFile("DeleteorAuditOrgs_listorgs.json")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		if err := json.Unmarshal(fileOrgJson, &body); err != nil {
+			panic(err)
+		}
+
+		OrgsLen := len(body.Resources)
+
+		fmt.Println("Number of Orgs: ", OrgsLen)
+
+		if OrgsLen != 0 {
+
+			for i := 0; i < OrgsLen; i++ {
+
+				var count, totalcount int
+				fmt.Println(" ")
+				fmt.Println("Org: ", body.Resources[i].Name)
+
+				for p := 0; p < LenProtectedOrgs; p++ {
+					//fmt.Println("Protected Org: ", ProtectedOrgs.Org[p], ",", body.Resources[i].Name)
+					if strings.Trim(ProtectedOrgs.Org[p], "") == strings.Trim(body.Resources[i].Name, "") {
+						count = 1
+					} else {
+						count = 0
+					}
+					totalcount = totalcount + count
+				}
+
+				if totalcount == 0 {
+
+					//fmt.Println("This is not Protected Org")
+
+					var orgscount, orgstotalcount int
+
+					for q := 0; q < LenList; q++ {
+						var OrgName string
+						if 	InitClusterConfigVals.ClusterDetails.EnableGitSubTree != true {
+							OrgName = list.OrgList[q].Name
+						} else {
+							OrgName = gitlist.OrgList[q].Name
+							//RepoName = gitlist.OrgList[i].Name
+						}
+						//fmt.Println("Org: ", list.OrgList[q], ",", body.Resources[i].Name)
+						if OrgName == body.Resources[i].Name {
+							orgscount = 1
+						} else {
+							orgscount = 0
+						}
+						orgstotalcount = orgstotalcount + orgscount
+					}
+
+					if orgstotalcount == 0 {
+						fmt.Println("Org has not be listed in Orglist: ")
+						fmt.Println("Auditing Org: ", body.Resources[i].Name)
+
+						target := exec.Command("cf", "t", "-o", body.Resources[i].Name)
+
+						if _, err := target.Output(); err == nil {
+							fmt.Println("command: ", target)
+							fmt.Println(target.Stdout)
+							if Audit == "delete" {
+								fmt.Println("DELETE!DELETE!")
+								fmt.Println("Deleting Org: ", body.Resources[i].Name)
+								delete := exec.Command("cf", "delete-org", body.Resources[i].Name, "-f")
+								if _, err := delete.Output(); err == nil {
+									fmt.Println("command: ", delete)
+									fmt.Println(delete.Stdout)
+								} else {
+									fmt.Println("command: ", delete)
+									fmt.Println("Err: ", delete.Stdout, delete.Stderr)
+								}
+							} else if Audit == "rename" {
+								fmt.Println("DELETE!DELETE!")
+								fmt.Println("Renaming Org: ", body.Resources[i].Name)
+								result, _ := regexp.MatchString("_tobedeleted", body.Resources[i].Name)
+								if result == true{
+									fmt.Println("Org already renamed")
+								} else {
+									rename := exec.Command("cf", "rename-org", body.Resources[i].Name, body.Resources[i].Name+"_tobedeleted")
+									if _, err := rename.Output(); err == nil {
+										fmt.Println("command: ", rename)
+										fmt.Println(rename.Stdout)
+									} else {
+										fmt.Println("command: ", rename)
+										fmt.Println("Err: ", rename.Stdout, rename.Stderr)
+									}
+								}
+							} else if Audit == "list" {
+								fmt.Println("DELETE!DELETE!")
+								fmt.Println("Org to be deleted: ", body.Resources[i].Name)
+							} else {
+								fmt.Println("Provide Valid Input")
+							}
+						} else {
+							fmt.Println("command: ", target)
+							fmt.Println("Err: ", target.Stdout, target.Stderr)
+						}
+					} else {
+						//fmt.Println("Org exists in Orgslist.yml: ", body.Resources[i].Name)
+					}
+				} else {
+					fmt.Println("This is a protected Org:", body.Resources[i].Name)
+				}
+			}
+		} else {
+			fmt.Println("No Orgs exist")
+		}
+
+	} else {
+		fmt.Println("command: ", getorgs)
+		fmt.Println("Err: ", getorgs.Stderr)
+	}
+	results := exec.Command("cf", "orgs")
+	if _, err := results.Output(); err != nil{
+		fmt.Println("command: ", results)
+		fmt.Println("Err: ", results.Stdout, err)
+	} else {
+		//fmt.Println("command: ", results)
+		fmt.Println(results.Stdout)
+	}
+	return err
+}
